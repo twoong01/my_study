c85dcfa50ca756c77b3d87678d38849b59b50e17
twoong01 <91011682+twoong01@users.noreply.github.com>
2023-02-11 21:36:55 +0900

    [Silver III] Title: N과 M (7), Time: 3024 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+n_lst = list(map(int, input().split()))
+n_lst = sorted(n_lst)
+isused = [False] * n
+arr = [0] * m
+def nPm(k):
+    if k == m:
+        for i in range(m):
+            print(arr[i], end=' ')
+        print()
+        return
+    else:
+        for idx, value in enumerate(n_lst):
+            if not isused[idx]:
+                arr[k] = value
+                nPm(k + 1)
+nPm(0)
