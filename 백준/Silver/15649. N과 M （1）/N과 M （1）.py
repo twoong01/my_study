e146bf7b4bbd2a3a7f1014af0d219245f6b678d5
twoong01 <91011682+twoong01@users.noreply.github.com>
2023-02-11 20:58:39 +0900

    [Silver III] Title: N과 M (1), Time: 236 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+isused = [False] * (n + 1)
+arr = [0] * m
+def nPm(k):
+    if k == m:
+        for i in range(m):
+            print(arr[i], end=' ')
+        print()
+        return
+    else:
+        for i in range(1, n + 1):
+            if not isused[i]:
+                arr[k] = i
+                isused[i] = True
+                nPm(k + 1)
+                isused[i] = False
+nPm(0)
