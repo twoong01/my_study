fa8d1c812c8258addc3ce2e49e2c7666b8cbc386
twoong01 <91011682+twoong01@users.noreply.github.com>
2023-02-12 23:27:32 +0900

    [Silver II] Title: N과 M (12), Time: 3008 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,18 @@
+def nPm(arr, k):
+    arr = sorted(arr)
+    isused = [False for _ in range(len(arr))]
+    def make(chosen, isused):
+        if len(chosen) == m:
+            if chosen == sorted(chosen):
+                print(*chosen)
+            return
+        for i in range(len(arr)):
+            if not isused[i] and (i == 0 or arr[i - 1] != arr[i] or isused[i - 1]):
+                chosen.append(arr[i])
+                make(chosen, isused)
+                chosen.pop()
+    make([], isused)
+
+n, m = map(int, input().split())
+n_lst = list(map(int, input().split()))
+nPm(n_lst, m)
