7322773e1a5e81f63e3333d05170e1ecbaa966f3
twoong01 <91011682+twoong01@users.noreply.github.com>
2023-03-03 00:12:27 +0900

    [Silver III] Title: Four Squares, Time: 56 ms, Memory: 33508 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,19 @@
+from math import sqrt
+
+
+def fourSquares(n):
+    if int(sqrt(n)) == sqrt(n):
+        return 1
+
+    for i in range(1, int(sqrt(n)) + 1):
+        if int(sqrt(n - i**2)) == sqrt(n - i**2):
+            return 2
+
+    for i in range(1, int(sqrt(n)) + 1):
+        for j in range(1, int(sqrt(n - i**2)) + 1):
+            if int(sqrt(n - i**2 - j**2)) == sqrt(n - i**2 - j**2):
+                return 3
+
+    return 4
+n = int(input())
+print(fourSquares(n))
