7a7027df8c4df8806f4b8b84e3ed25dbb8052a0a
twoong01 <91011682+twoong01@users.noreply.github.com>
2023-03-22 17:22:22 +0900

    [Silver I] Title: 카잉 달력, Time: 3392 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    m, n, x, y = map(int, input().split());
+    while (x <= m * n):
+        if x % n == y % n:
+            print(x)
+            break
+        x += m
+    else:
+        print(-1)
