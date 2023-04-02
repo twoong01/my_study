9e618b27d6a09f91890562f3dbcca44f13fe1acc
twoong01 <91011682+twoong01@users.noreply.github.com>
2023-04-02 17:12:03 +0900

    [unrated] Title: 과제 진행하기, Time: 86.29 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
@@ -0,0 +1,15 @@
+def solution(plans):
+    answer = []
+    plans = sorted(map(lambda x: [x[0], int(x[1][:2]) * 60 + int(x[1][3:]), int(x[2])], plans), key=lambda x: -x[1])
+    lst = []
+    while plans:
+        x = plans.pop()
+        for i, v in enumerate(lst):
+            if v[0] > x[1]:
+                lst[i][0] += x[2]
+        lst.append([x[1] + x[2], x[0]])
+    lst.sort()
+    ans = []
+    for i in lst:
+        ans.append(i[-1])
+    return ans
