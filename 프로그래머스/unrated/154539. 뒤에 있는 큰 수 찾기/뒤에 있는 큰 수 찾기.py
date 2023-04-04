408f95a5b3e16fc980214148fd1473100a50cef7
twoong01 <91011682+twoong01@users.noreply.github.com>
2023-04-04 10:27:34 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 195.59 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    n = len(numbers)
+    answer = [-1 for _ in range(n)]
+    stack = []
+    for i in range(n):
+        while stack and numbers[stack[-1]] < numbers[i]:
+            answer[stack.pop()] = numbers[i]
+        stack.append(i)
+    return answer
