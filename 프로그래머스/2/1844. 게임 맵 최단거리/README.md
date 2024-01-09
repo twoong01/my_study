f08522531edbe44fb72657fda4c0d053922b888d
twoong01 <91011682+twoong01@users.noreply.github.com>
2024-01-09 23:49:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.70 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 4.51 ms
+메모리: 10.2 MB, 시간: 4.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 2:3:48
+2024년 1월 2일 23:49:17
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,35 +1,24 @@
 from collections import deque
-
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
-    
-    # 상하좌우 방향을 나타내는 배열
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
-    # 시작 위치
-    start = (0, 0)
-    
-    # BFS를 위한 큐
-    queue = deque()
-    queue.append((start, 1))  # 시작 위치와 이동 횟수를 함께 저장
-    
-    while queue:
-        (x, y), steps = queue.popleft()
+    n = len(maps)
+    m = len(maps[0])
+    q = deque()
+    q.append([0, 0, 1])
+    while q:
+        x, y, step = q.popleft()
         
-        # 상대 팀 진영에 도착한 경우, 이동 횟수 반환
         if x == n - 1 and y == m - 1:
-            return steps
+            return step
         
-        for i in range(4):  # 상하좌우로 이동을 시도
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
-                # 이동 가능한 위치이면 큐에 추가하고 해당 위치를 벽으로 변경 (방문 처리)
-                queue.append(((nx, ny), steps + 1))
-                maps[nx][ny] = 0
-                
-    # 상대 팀 진영에 도착하지 못한 경우
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if not(0 <= nx < n and 0 <= ny < m): continue
+            if maps[nx][ny] == 0: continue
+            q.append([nx, ny, step + 1])
+            maps[nx][ny] = 0
     return -1
+            
