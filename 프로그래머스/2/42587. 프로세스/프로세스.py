6040fbf240b7ae301b5eededc561ef3025317e25
twoong01 <91011682+twoong01@users.noreply.github.com>
2024-02-01 01:19:43 +0900

    [level 2] Title: 프로세스, Time: 0.45 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,20 +1,21 @@
 from collections import deque
+
 def solution(priorities, location):
     answer = 0
     q = deque()
-    target = (priorities[location], location)
+    target = [priorities[location], location]
     result = []
     for i in range(len(priorities)):
-        q.append((priorities[i], i))
+        q.append([priorities[i], i])
     while q:
         pri, idx = q.popleft()
         if q:
-            mx = max(q)[0]
-            if pri < mx:
-                q.append((pri, idx))
+            first = max(q)[0]
+            if pri < first:
+                q.append([pri, idx])
             else:
-                result.append((pri, idx))
+                result.append([pri, idx])
         else:
-            result.append((pri, idx))        
+            result.append([pri, idx])
     answer = result.index(target) + 1
     return answer
