af24c60fb84caf3b051a6fb1d4cb5ad73f97987f
twoong01 <91011682+twoong01@users.noreply.github.com>
2024-02-01 16:32:04 +0900

    [level 2] Title: 타겟 넘버, Time: 7.20 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,17 @@
 from collections import deque
+
 def solution(numbers, target):
     answer = 0
     q = deque()
-    idx = 0
-    q.append([numbers[idx], idx])
-    q.append([(-1) * numbers[idx], idx])
+    q.append([numbers[0], 0])
+    q.append([(-1) * numbers[0], 0])
     while q:
         c_sum, c_idx = q.popleft()
         if c_idx == len(numbers) - 1:
             if c_sum == target:
                 answer += 1
         else:
-            if idx < len(numbers):
+            if c_idx < len(numbers) - 1:
                 q.append([c_sum + numbers[c_idx + 1], c_idx + 1])
                 q.append([c_sum - numbers[c_idx + 1], c_idx + 1])
     return answer
